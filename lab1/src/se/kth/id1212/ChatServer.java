481a959d26c6fd4d39a4a2e7eb9e87f282f4fc60
sylvia <sylviawxiuyee@outlook.com>
2020-10-29 11:29:35 +0100

    update the comments

--- lab1/src/se/kth/id1212/ChatServer.java
+++ lab1/src/se/kth/id1212/ChatServer.java
@@ -2,6 +2,13 @@ package se.kth.id1212;
 
 import java.net.ServerSocket;
 
+/**
+ * This ChatServer contains two threads one thread
+ * to each of the clients currently connected and
+ * sends the messages to other connected clients
+ * one thread to listen for new incoming connections
+ * from new clients.
+ */
 public class ChatServer {
 
 }
