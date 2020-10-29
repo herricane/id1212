8f1d36bb96dcb127b7882aa753f21ae2434149d2
韦晓仪 <weixiaoyi@weixiaoyis-MacBook-Pro.local>
2020-10-29 11:26:18 +0100

    update the comments

--- lab1/src/se/kth/id1212/ChatClient.java
+++ lab1/src/se/kth/id1212/ChatClient.java
@@ -1,7 +1,13 @@
 package se.kth.id1212;
 
+import java.io.*;
 import java.net.Socket;
 
+/**
+ * The ChatClient contains two threads,
+ * one to listen for incoming messages from the server
+ * and one to send messages to the server.
+ */
 public class ChatClient {
     Socket socket = new Socket();
 }
